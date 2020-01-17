1f4d772f62bfe7fcf556b9d285de4cd523e34f51
Turolol <sespa1234@gmail.com>
2020-01-17 18:13:42 +0100

    he creado un System println en el constructor

--- Github.java
+++ Github.java
@@ -6,28 +6,19 @@
  * @version (a version number or a date)
  */
 public class Github
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Github
-     */
-    public Github()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+{ /**
+   *Este metodo hace algo
+   *
+   *@param Y es un entero 
+   *
+   *@return devuelve un entero 
+   *
+   *@throws lanzar excepciones
+   */
+   
+   
+  public Github()
+  {
+    System.out.println("hola");
     }
 }
